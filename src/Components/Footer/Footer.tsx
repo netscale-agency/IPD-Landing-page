a120fe24f87c3f7443d1e21d3f06f43798de0b57
Matea <matea.ordulj@netscale.agency>
2024-01-11 17:04:30 +0100

    added links to footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Footer.module.css";
 import youtubeLogo from "../../Assets/youtubeLogo.svg";
 import instagramLogo from "../../Assets/instagramLogo.svg";
 import facebookLogo from "../../Assets/facebookLogo.svg";
+import YoutubeLogoGreen from "../../Assets/YoutubeLogoGreen.svg";
+import InstagramLogoGreen from "../../Assets/InstagramLogoGreen.svg";
+import FacebookLogoGreen from "../../Assets/FacebookLogoGreen.svg";
+import { Link, animateScroll as scroll } from "react-scroll";
 
 const Footer: React.FC = () => {
+  const [isYoutubeHovered, setYoutubeHovered] = useState(false);
+  const [isInstagramHovered, setInstagramHovered] = useState(false);
+  const [isFacebookHovered, setFacebookHovered] = useState(false);
+
   return (
     <div className={styles.footer}>
       <div className={styles.copyrightContainer}>
@@ -13,29 +21,108 @@ const Footer: React.FC = () => {
         </div>
       </div>
       <div className={styles.menuContainer}>
-        <a href="#" className={styles.navItem}>
+        <Link
+          to="oProgramu"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+          className={styles.navItem}
+        >
           O programu
-        </a>
-        <a href="#" className={styles.navItem}>
+        </Link>
+        <Link
+          to="sadrzaj"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+          className={styles.navItem}
+        >
           Sadržaj
-        </a>
-        <a href="#" className={styles.navItem}>
+        </Link>
+        <Link
+          to="benefiti"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+          className={styles.navItem}
+        >
           Benefiti
-        </a>
-        <a href="#" className={styles.navItem}>
+        </Link>
+        <Link
+          to="testimonials"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+          className={styles.navItem}
+        >
           Testimonials
-        </a>
-        <a href="#" className={styles.navItem}>
+        </Link>
+        <Link
+          to="faq"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+          className={styles.navItem}
+        >
           FAQ
-        </a>
-        <a href="#" className={styles.navItem}>
+        </Link>
+
+        <Link
+          to="contact"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+          className={styles.navItem}
+        >
           Kontakt
-        </a>
+        </Link>
       </div>
       <div className={styles.socialsContainer}>
-        <img src={youtubeLogo} alt="IconYT" className={styles.icon} />
-        <img src={instagramLogo} alt="IconInsta" className={styles.icon} />
-        <img src={facebookLogo} alt="IconFB" className={styles.icon} />
+        <a
+          href="https://www.youtube.com/@tomislavkuljis-ipd"
+          className={styles.iconLink}
+          target="_blank"
+        >
+          <img
+            src={isYoutubeHovered ? YoutubeLogoGreen : youtubeLogo}
+            alt="IconYT"
+            className={styles.icon}
+            onMouseEnter={() => setYoutubeHovered(true)}
+            onMouseLeave={() => setYoutubeHovered(false)}
+          />
+        </a>
+        <a
+          href="https://www.instagram.com/ipd_centar/?hl=en"
+          className={styles.iconLink}
+          target="_blank"
+        >
+          <img
+            src={isInstagramHovered ? InstagramLogoGreen : instagramLogo}
+            alt="IconInsta"
+            className={styles.icon}
+            onMouseEnter={() => setInstagramHovered(true)}
+            onMouseLeave={() => setInstagramHovered(false)}
+          />
+        </a>
+        <a
+          href="https://www.facebook.com/ipdcentar/"
+          className={styles.iconLink}
+          target="_blank"
+        >
+          <img
+            src={isFacebookHovered ? FacebookLogoGreen : facebookLogo}
+            alt="IconFB"
+            className={styles.icon}
+            onMouseEnter={() => setFacebookHovered(true)}
+            onMouseLeave={() => setFacebookHovered(false)}
+          />
+        </a>
       </div>
     </div>
   );
